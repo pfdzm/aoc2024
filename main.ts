88f367b279ab557b256ba7183705462c09a12cbf
Pablo Fernandez Michel <pfdzm@users.noreply.github.com>
2024-12-04 14:03:58 +0100

    day 4 pt 2

--- main.ts
+++ main.ts
@@ -146,8 +146,8 @@ async function day3(fileName: string) {
 const fail = (message: string) => {
   throw new Error(message);
 };
-const letters = "XMAS";
 
+const letters = "XMAS";
 async function day4(fileName: string) {
   console.log("\n", fileName, "\n");
   const input = await fs.readFile(fileName, "utf-8");
@@ -160,29 +160,119 @@ async function day4(fileName: string) {
     .map((line) => line.split(""));
   for (const [i, row] of matrix.entries()) {
     for (const [j, col] of row.entries()) {
-      if (col !== "X") {
+      if (col !== "A") {
+        // if (col !== "X") { // pt 1
         continue;
       }
       possibleStartLocations.push([i, j]);
     }
   }
 
-  const finders = [
-    findLeft,
-    findRight,
-    findTop,
-    findBottom,
-    findBottomLeft,
-    findBottomRight,
-    findTopLeft,
-    findTopRight,
+  type Finder = (possibleStart: number[], matrix: string[][]) => boolean;
+
+  // pt 1
+  // const finders = [
+  //   findLeft,
+  //   findRight,
+  //   findTop,
+  //   findBottom,
+  //   findBottomLeft,
+  //   findBottomRight,
+  //   findTopLeft,
+  //   findTopRight,
+  // ];
+
+  // pt 2
+  const findTopLeftAndBottomRight: Finder = ([row, col], matrix) => {
+    if (row < 1) {
+      // starting point is too far to the top
+      return false;
+    }
+    if (col < 1) {
+      // starting point is too far to the left
+      return false;
+    }
+    if (row > matrix.length - 1 - 1) {
+      // starting point is too far to the bottom
+      return false;
+    }
+    if (col > (matrix.at(0) ?? fail("expected to find a row")).length - 1 - 1) {
+      // starting point is too far to the right
+      return false;
+    }
+    const letters = ["M", "S"];
+    const topLeft = matrix[row - 1][col - 1];
+    const bottomRight = matrix[row + 1][col + 1];
+    if (!letters.includes(topLeft) || !letters.includes(bottomRight)) {
+      return false;
+    }
+
+    switch (topLeft) {
+      case "M":
+        if (bottomRight !== "S") {
+          return false;
+        }
+        return true;
+      case "S":
+        if (bottomRight !== "M") {
+          return false;
+        }
+        return true;
+      default:
+        return false;
+    }
+  };
+
+  const findTopRightAndBottomLeft: Finder = ([row, col], matrix) => {
+    if (row < 1) {
+      // starting point is too far to the top
+      return false;
+    }
+    if (col > (matrix.at(0) ?? fail("expected to find a row")).length - 1 - 1) {
+      // starting point is too far to the right
+      return false;
+    }
+    if (row > matrix.length - 1 - 1) {
+      // starting point is too far to the bottom
+      return false;
+    }
+    if (col < 1) {
+      // starting point is too far to the left
+      return false;
+    }
+    const letters = ["M", "S"];
+    const topRight = matrix[row - 1][col + 1];
+    const bottomLeft = matrix[row + 1][col - 1];
+    if (!letters.includes(topRight) || !letters.includes(bottomLeft)) {
+      return false;
+    }
+
+    switch (topRight) {
+      case "M":
+        if (bottomLeft !== "S") {
+          return false;
+        }
+        return true;
+      case "S":
+        if (bottomLeft !== "M") {
+          return false;
+        }
+        return true;
+      default:
+        return false;
+    }
+  };
+
+  // pt 2
+  const finders: Finder[] = [
+    findTopLeftAndBottomRight,
+    findTopRightAndBottomLeft,
   ];
 
   let matches = 0;
   for (const startLocation of possibleStartLocations) {
-    matches += finders
-      .map((find) => find(startLocation, matrix))
-      .reduce((prev, curr) => prev + (curr ? 1 : 0), 0);
+    matches += finders.every((find) => find(startLocation, matrix)) ? 1 : 0; // pt 2
+    // .reduce((prev, curr) => prev + (curr ? 1 : 0), 0); // pt 1
   }
   return matches;
 }
@@ -307,7 +397,7 @@ function findBottomLeft([row, col]: number[], matrix: string[][]): boolean {
     return false;
   }
   if (col < 3) {
-    // starting point is too far to the right
+    // starting point is too far to the left
     return false;
   }
   for (let i = 1; i < 4; i++) {
